8d46363299c4a13c683fee10d767e853757d42a8
Ronald J Kimball <rjk@tamias.net>
1998-03-10 12:02:13 +0000

    words.pl: add support for word index, miscellaneous changes
      'wordidx' file contains letter/offset pairs for 'wordlist'
      seeks to offset in 'wordlist' for each letter in sequence
      if unable to open 'wordidx', proceeds as before, reading each word
      should verify that 'wordidx' is newer than 'wordlist'

--- words.pl
+++ words.pl
@@ -4,28 +4,42 @@ use strict;
 
 use File::Basename;
 
-my($wordlist) = dirname($0) . "/wordlist";
+my $dir = dirname($0);
+my $wordlist = "$dir/wordlist";
+my $wordidx  = "$dir/wordidx";
 
 if (@ARGV < 2) {
 	warn "usage: $0 <min-length> <letters> ...\n";
 	exit;
 }
 
-my($minlen) = shift @ARGV;               # minimum word length
+my $minlen = shift @ARGV;                # minimum word length
 
 if ($minlen =~ /\D/) {
 	warn "$0: <min-length> must be a whole number\n";
 	exit 1;
 }
 
-open(DICT, $wordlist) or                   # open word list
-	die "Unable to open wordlist: $!";
+open(DICT, $wordlist) or                 # open word list
+	die "Unable to open $wordlist: $!\n";
 
-my($letters);
+my($idx, %idx);
+if (open(IDX, $wordidx)) {               # open word index
+	while(<IDX>) {
+		my($letter, $offset) = split;    # load letter/offset pairs
+		$idx{$letter} = $offset;
+		$idx = 1;                        # set index flag
+	}
+} else {
+	warn "Unable to open $wordidx: $!\nProceeding without index.\n";
+	$idx{0} = 0;
+}
+
+my $letters;
 foreach $letters (@ARGV) {               # for each letter sequence
 	print "-- $letters --\n";
 
-	my(%letters);
+	my %letters;
 
 	$letters = lc $letters;              # convert to lowercase
 	$letters =~ tr/a-z//cd;              # strip non-letter characters
@@ -34,27 +48,41 @@ foreach $letters (@ARGV) {               # for each letter sequence
 		$letters{$_}++;
 	}
 
-	seek(DICT, 0, 0);
+	my($letter, $word);
+  IDX:
+	foreach $letter ($idx ? sort keys %letters : 0) {
+                                         # for each letter in
+		                                 #   sequence if index loaded
+		                                 #   (0) otherwise
+		seek(DICT, $idx{$letter}, 0);    # seek to words beginning with letter
 
-	my($word);
-  WORD:
-	foreach $word (<DICT>) {             # for each word in list
-		chomp($word);
+	  WORD:
+		while (defined($word = <DICT>)) {
+                                         # for each word in list
+			next IDX if ($idx and substr($word, 0, 1) ne $letter);
+                                         # next letter index if index loaded
+			                             #   and done with current letter
 
-		next if (length($word) < $minlen);
-                                         # verify length
-		next if ($word !~ /^[\Q$letters\E]+$/);
+			next if $word =~ /^#/;       # skip comments
+
+			chomp($word);
+
+			next if (length($word) < $minlen);
+			                             # verify length
+			next if ($word !~ /^[\Q$letters\E]+$/);
                                          # verify letters used
 
-		my(%word);
-		foreach (split(//, $word)) {     # verify letter counts
-			$word{$_}++;
-			next WORD if ($word{$_} > $letters{$_});
-		}
+			my %word;
+			foreach (split(//, $word)) { # verify letter counts
+				$word{$_}++;
+				next WORD if ($word{$_} > $letters{$_});
+			}
 
-		print "$word\n";                 # success - print word
+			print "$word\n";             # success - print word
 
-	} # WORD: foreach $word (<DICT>)
+		} # WORD: while (defined($word = <DICT>))
+
+	} # IDX: foreach $letter (sort keys %idx)
 
 	print "\n";
 
