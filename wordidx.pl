5414e6afc4f7627d070786412683b6e15be55471
Ronald J Kimball <rjk@tamias.net>
1999-01-27 19:23:35 +0000

    wordidx.pl: Initial revision

---
+++ wordidx.pl
@@ -0,0 +1,26 @@
+#!/usr/local/bin/perl
+
+use strict;
+
+if (@ARGV < 2) {
+	die "usage: $0 wordfile idxfile\n";
+	exit 1;
+}
+
+my($wordfile, $idxfile) = @ARGV;
+
+open(WORDS, $wordfile) or die "Unable to open $wordfile: $!\n";
+open(IDX, ">$idxfile") or die "Unable to open $idxfile: $!\n";
+
+my $offset = 0;
+my $letter = '';
+
+while(<WORDS>) {
+	next if /^#/;
+	if (substr($_, 0, 1) ne $letter) {
+		$letter = substr($_, 0, 1);
+		print IDX "$letter $offset\n";
+	}
+	$offset += length $_;
+}
+
