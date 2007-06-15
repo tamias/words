9e082b190624e553bbdb0f81b5ab6029486258ad
Ronald J Kimball <rjk@tamias.net>
2007-06-15 18:21:56 +0000

    noose.pl: change indent to 2 spaces

--- noose.pl
+++ noose.pl
@@ -18,14 +18,14 @@ my $guesses = $opt_g || 20;
 my @words;
 
 while (<DICT>) {
-    chomp;
+  chomp;
 
-    next if $_ =~ /[^a-z]/;
-    push @words, $_ if length $_ == $length;
+  next if $_ =~ /[^a-z]/;
+  push @words, $_ if length $_ == $length;
 }
 
 if (not @words) {
-    die "No words!\n";
+  die "No words!\n";
 }
 
 my @letters = ('_') x $length;
@@ -33,62 +33,62 @@ my @letters = ('_') x $length;
 my %guessed;
 
 while (keys %guessed < $guesses and "@letters" =~ /_/) {
-    print "\n@letters\nGuessed: ", sort(keys %guessed), "\n? ";
+  print "\n@letters\nGuessed: ", sort(keys %guessed), "\n? ";
 
-    my $guess = lc <STDIN>;
-    chomp $guess;
+  my $guess = lc <STDIN>;
+  chomp $guess;
 
-    redo if length $guess > 1;
-    $guess = lc $guess;
-    redo if $guess =~ /[^a-z]/;
+  redo if length $guess > 1;
+  $guess = lc $guess;
+  redo if $guess =~ /[^a-z]/;
 
-    redo if exists $guessed{$guess};
+  redo if exists $guessed{$guess};
 
-    $guessed{$guess} = 1;
+  $guessed{$guess} = 1;
 
-    my $p = partition_words($guess);
+  my $p = partition_words($guess);
 
-    if ($p == @words) {
-        my $word = $words[rand @words];
+  if ($p == @words) {
+    my $word = $words[rand @words];
 
-        for (my $i = 0; $i < length $word; ++$i) {
-            if (substr($word, $i, 1) eq $guess) {
-                $letters[$i] = $guess;
-            }
-        }
-
-        $word =~ s/[^$guess]/[^$guess]/g;
-
-        my $q = partition_words($word);
-
-        splice(@words, $q);
-    } else {
-        splice(@words, 0, $p);
+    for (my $i = 0; $i < length $word; ++$i) {
+      if (substr($word, $i, 1) eq $guess) {
+        $letters[$i] = $guess;
+      }
     }
+
+    $word =~ s/[^$guess]/[^$guess]/g;
+
+    my $q = partition_words($word);
+
+    splice(@words, $q);
+  } else {
+    splice(@words, 0, $p);
+  }
 }
 
 if ("@letters" =~ /_/) {
-    print "\nOh no!\n";
+  print "\nOh no!\n";
 } else {
-    print "\n", @letters, "!\n";
+  print "\n", @letters, "!\n";
 }
 
 exit;
 
 sub partition_words {
-    my($pattern) = @_;
+  my($pattern) = @_;
 
-    $pattern = qr/$pattern/;
+  $pattern = qr/$pattern/;
 
-    my($p, $q);
+  my($p, $q);
 
-    for ($p = $q = 0; $q < @words; ++$q) {
-        if ($words[$q] =~ $pattern) {
-            @words[$p, $q] = @words[$q, $p]
-              unless $p == $q;
-            $p++;
-        }
+  for ($p = $q = 0; $q < @words; ++$q) {
+    if ($words[$q] =~ $pattern) {
+      @words[$p, $q] = @words[$q, $p]
+        unless $p == $q;
+      $p++;
     }
+  }
 
-    $p;
+  $p;
 }
