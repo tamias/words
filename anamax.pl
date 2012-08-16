619960e5a01a6bbc06f1539f59dd68e059e1e012
Ronald J Kimball <rjk@tamias.net>
2012-08-16 19:19:55 -0400

    Switch to 2-space indenting in anamax.pl

--- anamax.pl
+++ anamax.pl
@@ -17,28 +17,28 @@ my $dir = dirname($0);
 my $wordlist = $opt_w || "$dir/wordlist";
 
 if (@ARGV < 1) {
-    warn "usage: $0 <length>\n";
-    exit;
+  warn "usage: $0 <length>\n";
+  exit;
 }
 
 my $length = shift @ARGV;
 
 if ($length =~ /\D/) {
-    die "$0: <length> must be a whole number\n";
+  die "$0: <length> must be a whole number\n";
 }
 
 open(DICT, $wordlist) or                 # open word list
-    die "Unable to open $wordlist: $!\n";
+  die "Unable to open $wordlist: $!\n";
 
 my %count;
 
 while (<DICT>) {
-    chomp;
-    next if length != $length;
+  chomp;
+  next if length != $length;
 
-    my $canon = join '', sort split //;
+  my $canon = join '', sort split //;
 
-    push @{$count{$canon}}, $_;
+  push @{$count{$canon}}, $_;
 }
 
 close DICT;
@@ -49,9 +49,9 @@ my $i;
 my $limit = 2;
 for (sort { @{$count{$b}} <=> @{$count{$a}} ||
             $count{$a}[0] cmp $count{$b}[0] } keys %count) {
-    last if @{$count{$_}} < $limit;
-    if ($limit == 2 and $i++ > 10) {
-        $limit = @{$count{$_}};
-    }
-    print "@{$count{$_}}\n";
+  last if @{$count{$_}} < $limit;
+  if ($limit == 2 and $i++ > 10) {
+    $limit = @{$count{$_}};
+  }
+  print "@{$count{$_}}\n";
 }
