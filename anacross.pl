ec03fa20b5885de014ff35f5d3264cbab4990e71
Ronald J Kimball <rjk@tamias.net>
2012-08-16 23:07:39 -0400

    Switch to 2-space indenting in anacross.pl

--- anacross.pl
+++ anacross.pl
@@ -7,7 +7,7 @@ use strict;
 my $size = shift @ARGV;
 
 if (@ARGV != $size * 2) {
-    die "$0: wrong number of arguments.\n";
+  die "$0: wrong number of arguments.\n";
 }
 
 my @across = splice @ARGV, 0, $size;
@@ -16,28 +16,28 @@ my @down   = splice @ARGV, 0, $size;
 my $word;
 my @a_ltrs;
 foreach $word (@across) {
-    if (length $word != $size) {
-        die "'$word' is not of length $size.\n";
-    }
+  if (length $word != $size) {
+    die "'$word' is not of length $size.\n";
+  }
 
-    my %tmp;
-    foreach (split //, $word) {    
-        $tmp{$_}++;
-    }
-    push @a_ltrs, \%tmp;
+  my %tmp;
+  foreach (split //, $word) {    
+    $tmp{$_}++;
+  }
+  push @a_ltrs, \%tmp;
 }
 
 my @d_ltrs;
 foreach $word (@down) {
-    if (length $word != $size) {
-        die "'$word' is not of length $size.\n";
-    }
+  if (length $word != $size) {
+    die "'$word' is not of length $size.\n";
+  }
 
-    my %tmp;
-    foreach (split //, $word) {
-        $tmp{$_}++;
-    }
-    push @d_ltrs, \%tmp;
+  my %tmp;
+  foreach (split //, $word) {
+    $tmp{$_}++;
+  }
+  push @d_ltrs, \%tmp;
 }
 
 my $count = 0;
@@ -46,41 +46,41 @@ my $board;
 
 my ($a, $d);
 for $a (0 .. @a_ltrs - 1) {
-    for $d (0 .. @d_ltrs - 1) {
-        $board->[$a][$d] = '.';
-    }
+  for $d (0 .. @d_ltrs - 1) {
+    $board->[$a][$d] = '.';
+  }
 }
 
 my @share;
 
 while ($count != $oldcount && $count < $size * $size) {
-    $oldcount = $count;
-    for ($a = 0; $a < @a_ltrs; ++$a) {
-        for ($d = 0; $d < @d_ltrs; ++$d) {
-            next if $board->[$a][$d] ne '.';
+  $oldcount = $count;
+  for ($a = 0; $a < @a_ltrs; ++$a) {
+    for ($d = 0; $d < @d_ltrs; ++$d) {
+      next if $board->[$a][$d] ne '.';
 
-            @share = intersect($a_ltrs[$a], $d_ltrs[$d]);
-            if (@share == 1) {
-                $board->[$a][$d] = $share[0];
-                $count++;
-                $a_ltrs[$a]{$share[0]}--;
-                $d_ltrs[$d]{$share[0]}--;
-            }
-        }
+      @share = intersect($a_ltrs[$a], $d_ltrs[$d]);
+      if (@share == 1) {
+        $board->[$a][$d] = $share[0];
+        $count++;
+        $a_ltrs[$a]{$share[0]}--;
+        $d_ltrs[$d]{$share[0]}--;
+      }
     }
+  }
 }
 
 print "\n";
 foreach (@$board) {
-    print join '', @$_, "\n";
+  print join '', @$_, "\n";
 }
 print "\n";
 
 sub intersect {
-    my @ret;
-    foreach (keys %{$_[1]}) {
-        next unless $ {$_[1]}{$_};
-        push @ret, $_ if $ {$_[0]}{$_};
-    }
-    return @ret;
+  my @ret;
+  foreach (keys %{$_[1]}) {
+    next unless $ {$_[1]}{$_};
+    push @ret, $_ if $ {$_[0]}{$_};
+  }
+  return @ret;
 }
