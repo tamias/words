a096792c385e8dcfbaeb06cf5b6634bec662e0d5
Ronald J Kimball <rjk@tamias.net>
2012-08-23 22:44:52 -0400

    Modernize orphan.pl
    Use strict
    Use warnings instead of -w
    Switch to 3-arg open and lexical filehandles

--- orphan.pl
+++ orphan.pl
@@ -1,35 +1,40 @@
-#!/usr/local/bin/perl -w
+#!/usr/local/bin/perl
 
 # find ladder orphans; words that cannot be changed into another word
 # by changing a single letter
 
-$length = shift || 4;
+use strict;
+use warnings;
 
-open(WORDS, "wordlist") or die "Can't open: $!\n";
+my $length = shift || 4;
+
+open(my $words_fh, "<", "wordlist") or die "Can't open: $!\n";
+
+my (@words, %steps);
 
 WORD:
-while (defined($word = <WORDS>)) {
+while (my $word = <$words_fh>) {
   chomp $word;
   next WORD
     if length $word != $length;
   push @words, $word;
-  for ($i=0; $i<length $word; ++$i) {
-    $step = $word;
+  for (my $i=0; $i<length $word; ++$i) {
+    my $step = $word;
     substr($step, $i, 1) = '.';
     $steps{$step}++;
   }
 }
 
-close(WORDS);
+close($words_fh);
 
-$count = 0;
+my $count = 0;
 
 WORD:
-foreach $word (@words) {
+foreach my $word (@words) {
   next WORD
     if length $word != $length;
-  for ($i=0; $i<length $word; ++$i) {
-    $step = $word;
+  for (my $i=0; $i<length $word; ++$i) {
+    my $step = $word;
     substr($step, $i, 1) = '.';
     next WORD
       if $steps{$step} > 1;
