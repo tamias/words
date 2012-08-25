c9ac3fd2b9fd8d4203ae5c82b448a1e8f134503b
Ronald J Kimball <rjk@tamias.net>
2012-08-24 21:42:05 -0400

    Modernize rotreverse.pl, switch to 2-space indenting

--- rotreverse.pl
+++ rotreverse.pl
@@ -1,22 +1,20 @@
-#!/usr/local/bin/perl -w
+#!/usr/local/bin/perl
 
 # find words that can be rotated 13 letters and reversed to form another
 # (or the same) word
 
 use strict;
-
-$| = 1;
+use warnings;
 
 my %words;
 my @match;
 
 while (<>) {
-    chomp;
-    $words{$_} = 1;
-    my $rr = reverse $_;
-    $rr =~ tr/a-zA-Z/n-za-mN-ZA-M/;
-    push @match, "$rr $_" if $words{$rr};
+  chomp;
+  $words{$_} = 1;
+  my $rr = reverse $_;
+  $rr =~ tr/a-zA-Z/n-za-mN-ZA-M/;
+  push @match, "$rr $_" if $words{$rr};
 }
 
 print join "\n", (sort @match), '';
-
